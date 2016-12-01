fe96b0a7d693a82736e2d3771d164d4035becb53
Stephan Lensky <stephan.lensky@gmail.com>
2016-12-01 13:13:29 -0500

    Add session timeout support

--- focus/auth.py
+++ focus/auth.py
@@ -1,8 +1,10 @@
 #!flask/bin/python
 import requests
 import sys
+import time
 
 student_sessions = {}
+
 
 def login(u, p, url):
     data = {
@@ -11,12 +13,16 @@ def login(u, p, url):
     }
     r = requests.post(url, data)
     if r.json()['success']:
-        student_sessions[r.cookies['PHPSESSID']] = u
+        student_sessions[r.cookies['PHPSESSID']] = u, time.time()
         return r.cookies['PHPSESSID']
     else:
         return ()
 
+
 def is_valid_session(sess_id):
-    if sess_id in student_sessions:
+    if sess_id in student_sessions and time.time() < student_sessions[sess_id][1] + (20 * 60):
+        student_sessions[sess_id] = student_sessions[sess_id][0], time.time()
         return True
-    return False
+    else:
+        student_sessions.pop(sess_id)
+        return False
