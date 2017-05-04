e1fd884c30b92185d710a654031372bbfc3ca3c8
Stephan Lensky <stephan.lensky@gmail.com>
2017-05-04 09:23:28 -0400

    Final fix for session timeout + session timeout cookie

--- focus/app.py
+++ focus/app.py
@@ -1,11 +1,11 @@
 #!flask/bin/python
 import sys
-
+from time import time
 from flask import Flask, jsonify, abort, request, make_response
 import requests
 import base64
 from focus import parser
-from focus.session import Session, find_session, session_expired
+from focus.session import Session, find_session
 from focus.json_simplify import simplify_final_grades
 from calendar import monthrange
 from datetime import date
@@ -45,6 +45,10 @@ def sign_request(request):
     hash = hmac.new(secret, digest.encode('utf-8'), hashlib.sha1).hexdigest()
     request['signature'] = hash
 
+def set_cookies(r, s):
+    r.set_cookie('PHPSESSID', s.sess_id)
+    r.set_cookie('session_timeout', str(int(s.timeout)))
+
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify( { 'error': 'Bad request' } ), 400)
@@ -70,9 +74,14 @@ def internal_server_error(error):
 def session():
     if request.method == 'GET':
         s = find_session(request.cookies.get('PHPSESSID'), sessions)
-        if s is None or session_expired(s):
+        if s is None or s.expired():
             abort(403)
-        return jsonify(s.dictify_for_user())
+        resp = jsonify({
+            'timeout': int(s.timeout),
+            'username': s.user
+        })
+        set_cookies(resp, s)
+        return resp
     elif request.method == 'POST':
         if not request.json or not 'username' in request.json or not 'password' in request.json:
             abort(400)
@@ -86,8 +95,11 @@ def session():
         if r.status_code == 200 and r.json()['success']:
             s = Session(request.json.get('username'), r.cookies['PHPSESSID'])
             sessions.append(s)
-            resp = jsonify(s.dictify_for_user())
-            resp.set_cookie('PHPSESSID', s.sess_id)
+            resp = jsonify({
+                'timeout': int(s.timeout),
+                'username': s.user
+            })
+            set_cookies(resp, s)
             return resp
         elif r.status_code == 200:
             abort(401)
@@ -96,7 +108,7 @@ def session():
 
     elif request.method == 'PUT':
         s = find_session(request.cookies.get('PHPSESSID'), sessions)
-        if s is None or session_expired(s):
+        if s is None or s.expired():
             abort(403)
         if not request.json or not 'year' in request.json or not 'mp_id' in request.json \
                 or not isinstance(request.json['year'], int) or not isinstance(request.json['mp_id'], int):
@@ -166,24 +178,30 @@ def session():
                 abort(500)
             parsed = parser.parse_portal(r.text)
 
-
-        return jsonify(dict(parsed, **parser.get_marking_periods(r.text)))
+        s.last_accessed = time()
+        resp = jsonify(dict(parsed, **parser.get_marking_periods(r.text)))
+        set_cookies(resp, s)
+        return resp
 
 
 @app.route(api_url + 'portal', methods = ['GET'])
 def portal():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    print(s.expired())
+    if s is None or s.expired():
         abort(403)
     r = requests.get(urls['portal'], cookies=request.cookies)
     if r.status_code != 200:
         abort(500)
-    return jsonify(dict(parser.parse_portal(r.text), **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(parser.parse_portal(r.text), **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'courses', methods = ['GET'])
 def courses():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
     r = requests.get(urls['portal'] + str(id), cookies=request.cookies)
     if r.status_code != 200:
@@ -199,34 +217,43 @@ def courses():
         parsed['days'] = c['days']
         d['courses'][parsed['id']] = parsed
 
-    return jsonify(dict(d, **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(d, **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'courses/<id>', methods = ['GET'])
 def course(id):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
     r = requests.get(urls['course_pre'] + str(id), cookies=request.cookies)
     if r.status_code == 404:
         abort(404)
     elif r.status_code != 200:
         abort(500)
-    return jsonify(dict(parser.parse_course(r.text), **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(parser.parse_course(r.text), **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'schedule', methods = ['GET'])
 def schedule():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
     r = requests.get(urls['schedule'], cookies=request.cookies)
     if r.status_code != 200:
         abort(500)
-    return jsonify(dict(parser.parse_schedule(r.text), **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(parser.parse_schedule(r.text), **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'calendar/<int:year>', methods = ['GET'])
 def calendar_by_year(year):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     query = "month={}&year={}".format(1, year)
@@ -243,12 +270,15 @@ def calendar_by_year(year):
             abort(500)
         parsed = parser.parse_calendar(r.text)
         d['events'] = d['events'] + parsed['events']
-    return jsonify(dict(d), **parser.get_marking_periods(r.text))
+    s.last_accessed = time()
+    resp = jsonify(dict(d), **parser.get_marking_periods(r.text))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'calendar/<int:year>/<int:month>', methods = ['GET'])
 def calendar_by_month(year, month):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
     if not month > 0 or not month < 13:
         abort(400)
@@ -257,12 +287,15 @@ def calendar_by_month(year, month):
     r = requests.get(urls['calendar_pre'] + query, cookies=request.cookies)
     if r.status_code != 200:
         abort(500)
-    return jsonify(dict(parser.parse_calendar(r.text), **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(parser.parse_calendar(r.text), **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'calendar/<int:year>/<int:month>/<int:day>', methods = ['GET'])
 def calendar_by_day(year, month, day):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
     if month < 1 or month > 12 or day < monthrange(year, month)[0] or day > monthrange(year, month)[1]:
         abort(400)
@@ -274,12 +307,15 @@ def calendar_by_day(year, month, day):
     parsed = parser.parse_calendar(r.text)
     parsed['events'] = [i for i in parsed['events'] if parse(i['date']).day == day]
     parsed['day'] = day
-    return jsonify(dict(parsed, **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(parsed, **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'calendar/assignments/<id>', methods = ['GET'])
 def assignment(id):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     r = requests.get(urls['assignment_pre'] + str(id), cookies=request.cookies)
@@ -288,13 +324,16 @@ def assignment(id):
     ret = parser.parse_calendar_event(r.text)
     ret['id'] = str(id)
     if ret:
-        return jsonify(ret)
+        s.last_accessed = time()
+        resp = jsonify(ret)
+        set_cookies(resp, s)
+        return resp
     abort(400)
 
 @app.route(api_url + 'calendar/occasions/<id>', methods = ['GET'])
 def occasion(id):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     r = requests.get(urls['event_pre'] + str(id), cookies=request.cookies)
@@ -303,13 +342,16 @@ def occasion(id):
     ret = parser.parse_calendar_event(r.text)
     ret['id'] = str(id)
     if ret:
-        return jsonify(ret)
+        s.last_accessed = time()
+        resp = jsonify(ret)
+        set_cookies(resp, s)
+        return resp
     abort(400)
 
 @app.route(api_url + 'demographic', methods = ['GET'])
 def demographic():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
     r = requests.get(urls['demographic'], cookies=request.cookies)
     if r.status_code != 200:
@@ -317,32 +359,41 @@ def demographic():
     ret = parser.parse_demographic(r.text)
     img = requests.get(urls['tld'] + ret[1].replace('../', ''), cookies=request.cookies)
     ret[0]['picture'] = base64.b64encode(img.content).decode('utf-8')
-    return jsonify(dict(ret[0], **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(ret[0], **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'address', methods = ['GET'])
 def address():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
     r = requests.get(urls['address'], cookies=request.cookies)
     if r.status_code != 200:
         abort(500)
-    return jsonify(dict(parser.parse_address(r.text), **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(parser.parse_address(r.text), **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'referrals', methods = ['GET'])
 def referrals():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
     r = requests.get(urls['referrals'], cookies=request.cookies)
     if r.status_code != 200:
         abort(500)
-    return jsonify(dict(parser.parse_referrals(r.text), **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(parser.parse_referrals(r.text), **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'referrals/<id>', methods = ['GET'])
 def referral(id):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
     r = requests.get(urls['referrals'], cookies=request.cookies)
     if r.status_code != 200:
@@ -357,12 +408,15 @@ def referral(id):
     if target == None:
         abort(404)
 
-    return jsonify(dict(target, **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(target, **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'exams', methods = ['GET'])
 def exams():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     # calling the API does not work if you don't get this page first (focus plz)
@@ -370,6 +424,7 @@ def exams():
         r = requests.get(urls['final_grades'], cookies=request.cookies)
         s.can_invoke_api = True
         s.student_id = parser.get_student_id(r.text)
+        s.last_accessed = time()
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
@@ -387,12 +442,14 @@ def exams():
         abort(500)
     d = simplify_final_grades(r.json(), 'exams')
 
-    return jsonify(d)
+    resp = jsonify(d)
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'exams/<id>', methods = ['GET'])
 def exam(id):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     # calling the API does not work if you don't get this page first (focus plz)
@@ -400,6 +457,7 @@ def exam(id):
         r = requests.get(urls['final_grades'], cookies=request.cookies)
         s.can_invoke_api = True
         s.student_id = parser.get_student_id(r.text)
+        s.last_accessed = time()
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
@@ -419,13 +477,15 @@ def exam(id):
 
     for e in d['exams']:
         if e['id'] == id:
-            return jsonify(e)
+            resp = jsonify(e)
+            set_cookies(resp, s)
+            return resp
     abort(404)
 
 @app.route(api_url + 'final_grades', methods = ['GET'])
 def final_grades():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     # calling the API does not work if you don't get this page first (focus plz)
@@ -433,6 +493,7 @@ def final_grades():
         r = requests.get(urls['final_grades'], cookies=request.cookies)
         s.can_invoke_api = True
         s.student_id = parser.get_student_id(r.text)
+        s.last_accessed = time()
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
@@ -450,12 +511,14 @@ def final_grades():
         abort(500)
     d = simplify_final_grades(r.json(), 'grades')
 
-    return jsonify(d)
+    resp = jsonify(d)
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'final_grades/<id>', methods = ['GET'])
 def final_grade(id):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     # calling the API does not work if you don't get this page first (focus plz)
@@ -463,6 +526,7 @@ def final_grade(id):
         r = requests.get(urls['final_grades'], cookies=request.cookies)
         s.can_invoke_api = True
         s.student_id = parser.get_student_id(r.text)
+        s.last_accessed = time()
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
@@ -482,13 +546,15 @@ def final_grade(id):
 
     for g in d['grades']:
         if g['id'] == id:
-            return jsonify(g)
+            resp = jsonify(g)
+            set_cookies(resp, s)
+            return resp
     abort(404)
 
 @app.route(api_url + 'semester_grades', methods = ['GET'])
 def semester_grades():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     # calling the API does not work if you don't get this page first (focus plz)
@@ -496,6 +562,7 @@ def semester_grades():
         r = requests.get(urls['final_grades'], cookies=request.cookies)
         s.can_invoke_api = True
         s.student_id = parser.get_student_id(r.text)
+        s.last_accessed = time()
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
@@ -513,12 +580,14 @@ def semester_grades():
         abort(500)
     d = simplify_final_grades(r.json(), 'grades')
 
-    return jsonify(d)
+    resp = jsonify(d)
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'semester_grades/<id>', methods = ['GET'])
 def semester_grade(id):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     # calling the API does not work if you don't get this page first (focus plz)
@@ -526,6 +595,7 @@ def semester_grade(id):
         r = requests.get(urls['final_grades'], cookies=request.cookies)
         s.can_invoke_api = True
         s.student_id = parser.get_student_id(r.text)
+        s.last_accessed = time()
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
@@ -545,13 +615,15 @@ def semester_grade(id):
 
     for g in d['grades']:
         if g['id'] == id:
-            return jsonify(g)
+            resp = jsonify(g)
+            set_cookies(resp, s)
+            return resp
     abort(404)
 
 @app.route(api_url + 'quarter_grades', methods = ['GET'])
 def quarter_grades():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     # calling the API does not work if you don't get this page first (focus plz)
@@ -559,6 +631,7 @@ def quarter_grades():
         r = requests.get(urls['final_grades'], cookies=request.cookies)
         s.can_invoke_api = True
         s.student_id = parser.get_student_id(r.text)
+        s.last_accessed = time()
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
@@ -576,12 +649,14 @@ def quarter_grades():
         abort(500)
     d = simplify_final_grades(r.json(), 'grades')
 
-    return jsonify(d)
+    resp = jsonify(d)
+    set_cookies(resp, s)
+    return resp
 
 @app.route(api_url + 'quarter_grades/<id>', methods = ['GET'])
 def quarter_grade(id):
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
 
     # calling the API does not work if you don't get this page first (focus plz)
@@ -589,6 +664,7 @@ def quarter_grade(id):
         r = requests.get(urls['final_grades'], cookies=request.cookies)
         s.can_invoke_api = True
         s.student_id = parser.get_student_id(r.text)
+        s.last_accessed = time()
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
@@ -608,18 +684,23 @@ def quarter_grade(id):
 
     for g in d['grades']:
         if g['id'] == id:
-            return jsonify(g)
+            resp = jsonify(g)
+            set_cookies(resp, s)
+            return resps
     abort(404)
 
 @app.route(api_url + 'absences', methods = ['GET'])
 def absences():
     s = find_session(request.cookies.get('PHPSESSID'), sessions)
-    if s is None or session_expired(s):
+    if s is None or s.expired():
         abort(403)
     r = requests.get(urls['absences'], cookies=request.cookies)
     if r.status_code != 200:
         abort(500)
-    return jsonify(dict(parser.parse_absences(r.text), **parser.get_marking_periods(r.text)))
+    s.last_accessed = time()
+    resp = jsonify(dict(parser.parse_absences(r.text), **parser.get_marking_periods(r.text)))
+    set_cookies(resp, s)
+    return resp
 
 
 if __name__ == '__main__':
--- focus/session.py
+++ focus/session.py
@@ -1,31 +1,27 @@
 import time
 
 class Session(object):
-    timeout = 20 * 60 # in seconds
+    time_limit = 20 * 60 # in seconds
     student_id = None
     can_invoke_api = False
+
 
     def __init__(self, user, sess_id):
         self.user = user
         self.sess_id = sess_id
         self.last_accessed = time.time()
 
-    def __get_expires(self):
-        t = self.last_accessed + self.timeout
-        self.last_accessed = time.time()
-        return t
+    def __get_timeout(self):
+        return self.last_accessed + self.time_limit
 
-    def dictify_for_user(self) -> dict:
-        return {
-            'timeout': self.last_accessed + self.timeout,
-            'username': self.user
-        }
+    def expired(self):
+        return self.__get_timeout() < time.time()
 
-    expire_time = property(__get_expires)
+    timeout = property(__get_timeout)
 
 def is_valid_session(sess_id, sessions):
     s = find_session(sess_id, sessions)
-    if s is not None and session_expired(s):
+    if s is not None and not s.expired():
         return True
     return False
 
@@ -34,8 +30,3 @@ def find_session(sess_id, sessions: list) -> Session:
         if s.sess_id == sess_id:
             return s
     return None
-
-def session_expired(s):
-    if s.expire_time < time.time():
-        return True
-    return False
